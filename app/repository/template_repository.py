37dd082810857cb79472d017060ad092549d4291
Hari Kishore <hbalepalli@eightfold.ai>
2025-05-11 19:48:10 +0530

    udpate status

--- app/repository/template_repository.py
+++ app/repository/template_repository.py
@@ -18,8 +18,37 @@ class TemplateRepository(BaseRepository[Template]):
         return template
     
     def update_summary_status(self, template_id, status):
-        template = self.db.query(Template).filter(Template.id == template_id).first()
+        template = self.get_by_id(template_id)
         template.summary_status = status
         self.db.commit()
         self.db.refresh(template)
         return template
+        
+    def update_character_arc_status(self, template_id, status):
+        template = self.get_by_id(template_id)
+        template.character_arc_status = status
+        self.db.commit()
+        self.db.refresh(template)
+        return template
+    
+    def update_plot_beats_status(self, template_id, status):
+        template = self.get_by_id(template_id)
+        template.plot_beats_status = status
+        self.db.commit()
+        self.db.refresh(template)
+        return template
+        
+    def update_character_arc_template_status(self, template_id, status):
+        template = self.get_by_id(template_id)
+        template.character_arc_template_status = status
+        self.db.commit()
+        self.db.refresh(template)
+        return template
+        
+    def update_plot_beat_template_status(self, template_id, status):
+        template = self.get_by_id(template_id)
+        template.plot_beat_template_status = status
+        self.db.commit()
+        self.db.refresh(template)
+        return template
+        
