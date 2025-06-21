62a44fa86c8ded87abfe1af9dacbc62ba0be505e
M SUJITH REDDY <50213663+MSR806@users.noreply.github.com>
2025-06-21 20:31:52 +0530

    feat: refactor metrics collection with statsd integration and constants module (#5)
    * feat: refactor metrics collection with statsd integration and constants module
    
    * env edited

---
+++ app/constants/metrics.py
@@ -0,0 +1,13 @@
+class Constants:
+    class Metric:
+        PREFIX = "vaani"
+        HUNDRED_SAMPLING_RATE = 1
+        INCREMENT_COUNT = 1
+        API_LATENCY = "request_latency"
+        API_COUNT = "request_count"
+
+    class Tag:
+        PATH = "path"
+        METHOD = "method"
+        CODE = "code"
+        STAGE = "stage"
